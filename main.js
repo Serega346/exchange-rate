8f1fa8e5321c35f10f5caf6fd2d87cc683460daa
Serhii Khomyn <serghomyn@gmail.com>
2020-01-26 14:02:30 +0200

    refresh

--- main.js
+++ main.js
@@ -5,29 +5,57 @@ var showCost = document.querySelector(".cost");
 var showDate = document.querySelector(".date");
 
 var people = function () {
+
   var xhttp = new XMLHttpRequest();
+
   xhttp.open("GET", "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json", true);
   xhttp.responseType = 'json';
+
   xhttp.onload = function () {
+
     let status = xhttp.status;
+
     if (status == 200) {
+
+       showName.innerHTML = '';
+       showCost.innerHTML = '';
+       showValue.innerHTML = '';
+       showDate.innerHTML = '';
+
       for (let name of xhttp.response) {
-        showName.innerHTML += `<p>${name.txt}</p>`
+
+        showName.innerHTML += `<p>${name.txt}</p>`;
+
       };
+      
       for (let value of xhttp.response) {
-        showValue.innerHTML += `<p>${value.cc}</p>`
+
+         showValue.innerHTML += `<p>${value.cc}</p>`;
+
       };
+
       for (let cost of xhttp.response) {
-        showCost.innerHTML += `<p>${cost.rate}</p>`
+
+        showCost.innerHTML += `<p>${cost.rate}</p>`;
+
       };
+
       for (let date of xhttp.response) {
-        showDate.innerHTML += `<p>${date.exchangedate}</p>`
+
+        showDate.innerHTML += `<p>${date.exchangedate}</p>`;
+
       };
+
     };
+
   };
+
   xhttp.send();
+
 };
 
 var show = function () {
+
   people();
+
 };
